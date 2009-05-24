d7c52ad5b9233be75f52cba2a17c10b5e9666351
Vojtech Horky <vojtech.horky@gmail.com>
2009-05-24 16:34:14 +0000

    Chang the `main' function to run parsing

--- chemface/Chemface.java
+++ chemface/Chemface.java
@@ -3,40 +3,20 @@ package chemface;
 public class Chemface {
 
 
-public static void main(String[] args) {
-	java.awt.image.BufferedImage img = 
-		new java.awt.image.BufferedImage(1000, 1000, java.awt.image.BufferedImage.TYPE_4BYTE_ABGR);
-	//Node node = new Node("H_{2}^{+I}S^{+VI}O_4^{-II}", new java.awt.Font("Arial", java.awt.Font.PLAIN, 160));
-	Node node = new Node("H", new java.awt.Font("Arial", java.awt.Font.PLAIN, 160));
-	//node.render();
-	Node node2 = new Node("H2", new java.awt.Font("Arial", java.awt.Font.PLAIN, 160));
-	//node2.render();
-	Node node3 = new Node("N3", new java.awt.Font("Arial", java.awt.Font.PLAIN, 160));
-	//node3.render();
+public static void main(String[] args) throws java.io.IOException {
+	SmilesLexer lexer = new SmilesLexer();
+	lexer.setSource("COC");
 	
-	RenderingOptions opts = RenderingOptions.getInstance();
-	try {
-		node2.render();
-		javax.imageio.ImageIO.write(node2.getImage(), "png", new java.io.File("tmp.png"));
-	} catch (java.io.IOException e) {
-	} //*/
+	SmilesParser parser = new SmilesParser(lexer);
+	parser.setDebugLevel(1);
+	boolean parseOkay = parser.parse();
+	if (!parseOkay) {
+		System.out.println("Parsing went gaga!");
+		return;
+	}
 	
-	
-	PositionedNode n = new PositionedNode(node);
-	n.fixed = true;
-	PositionedNode n2 = new PositionedNode(node2);
-	PositionedNode n3 = new PositionedNode(node3);
-	
-	Bond bond1 = new Bond();
-	Bond bond2 = new Bond();
-	
-	NodePlacer placer = new NodePlacer();
-	
-	placer.addVertex(n);
-	placer.addVertex(n2);
-	placer.addVertex(n3);
-	placer.addEdge(n, n2, bond1);
-	placer.addEdge(n, n3, bond2);
+	System.out.println("Parsing went fine!");
+	NodePlacer placer = parser.getNodes();
 	
 	placer.findOptimalPlacement();
 	placer.copyNodePositionsToEdges();
@@ -48,7 +28,8 @@ public static void main(String[] args) {
 		javax.imageio.ImageIO.write(renderer.getImage(), "png", new java.io.File("out.png"));
 	} catch (java.io.IOException e) {
 		System.out.println(e.toString());
-	} 
+	}
+	
 }
 
 
