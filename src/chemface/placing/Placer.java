e1ad85b2a56ba20771cb92335400f76d966dcadd
Vojtech Horky <vojta.horky@seznam.cz>
2009-09-24 15:55:08 +0200

    Add documentation comments to Placer

--- src/chemface/placing/Placer.java
+++ src/chemface/placing/Placer.java
@@ -5,12 +5,29 @@ import chemface.*;
 public interface Placer {
 
 /**
+ * Sets reactant to be used for placing.
+ *
  * @warning This method must copy the reactant.
  * 
  */
 public void setReactant(final Reactant r);
-public boolean placeOptimally();
+
+/**
+ * Returns placed reactant.
+ * It does not make much sense to call this method without prior call
+ * to placeOptimally() or when this method returns false.
+ * 
+ */
 public Reactant getReactant();
+
+/**
+ * Finds the optimal placement.
+ * 
+ * @return Success of the quest for optimal placement.
+ * 
+ */
+public boolean placeOptimally();
+
 
 }
 
